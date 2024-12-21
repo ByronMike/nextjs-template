ca39dcbcb0d3b1c49795d9940dad4305a7dc8b68
Michael Auger <mauger@laprovence.com>
2024-12-21 15:24:52 +0100

    feat: setup demo features and install sass

---
+++ src/components/demo/Counter.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import React from 'react';
+import useCounter from '@/hooks/demo/useCounter';
+
+function CounterComponent() {
+  const { count, increment, decrement, reset } = useCounter(10);
+
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+}
+
+export default CounterComponent;
